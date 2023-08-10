6d656907d6633bbc0d2b7695522e69918faaf0f7
Michael Mwangi <michaelmichire2016@gmail.com>
2023-08-10 07:07:46 -0700

    A program that prints a line using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* using puts to print a line */
+/* using puts to print a line 
+ *Return: 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle,");
 return (0);
 }
