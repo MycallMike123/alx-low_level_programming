8b7c8bd41758c94a952bf5bf64a4cf2fd1a477ba
Michael Mwangi <michaelmichire2016@gmail.com>
2023-08-10 10:40:51 -0700

    A C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point
+ * main - the entry point
+ * printing exact words but return a std
+ * return: 1 (Stantard error)
 */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return 1;
+return (1);
 }
