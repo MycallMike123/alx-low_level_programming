2607c0395f824ba39bdc83af1ccd84095eaffd18
Michael Mwangi <michaelmichire2016@gmail.com>
2023-08-17 11:01:52 -0700

    A function that prints an integer not using long, arrays or pointers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer using _putchar
+ * @n: The integer to be printed
+ */
+
+void print_number(int n)
+{
+	int div = 1;
+	int temp = n;
+	int digit;
+
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (temp > 9)
+	{
+		div *= 10;
+		temp /= 10;
+	}
+
+	while (div > 0)
+	{
+		digit = n / div;
+		_putchar(digit + '0');
+		n -= digit * div;
+		div /= 10;
+	}
+}
