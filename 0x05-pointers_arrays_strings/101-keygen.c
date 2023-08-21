25e3d7e761e94eb5e1218ddf93d869ddb56c4976
Michael Mwangi <michaelmichire2016@gmail.com>
2023-08-21 10:13:41 -0700

    A  program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -17,21 +17,25 @@ int main(void)
 	const char chars1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	const char chars2[] = "0123456789";
 	char passwd[PASSWORD_LENGTH + 1];
+	const char success[] = "Tada! Congrats"
 	int i;
 
 	srand(time(NULL));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		if (i < 10)
-			passwd[i] = chars[rand() % (sizeof(chars) - 1)];
-		else if (i < 20)
-			passwd[i] = chars1[rand() % (sizeof(chars1) - 1)];
-		else
-			passwd[i] = chars2[rand() % (sizeof(chars2) - 1)];
-	}
+	do {
+		for (i = 0; i < PASSWORD_LENGTH; i++)
+		{
+			if (i < 10)
+				passwd[i] = chars[rand() % (sizeof(chars) - 1)];
+			else if (i < 20)
+				passwd[i] = chars1[rand() % (sizeof(chars1) - 1)];
+			else
+				passwd[i] = chars2[rand() % (sizeof(chars2) - 1)];
+		}
 
-	passwd[PASSWORD_LENGTH] = '\0';
+		passwd[PASSWORD_LENGTH] = '\0';
+
+	} while (strcmp(passwd, expected) != 0);
 
 	printf("%s\n", passwd);
 
