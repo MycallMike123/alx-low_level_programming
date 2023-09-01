7ffbbffb704ad869c62c3ab62b8c82323a17214a
Michael Mwangi <michaelmichire2016@gmail.com>
2023-09-01 06:26:56 -0700

    A program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _new_name - prints its name
+ * main - prints its name
  * @argc: number of command line arguments
  * @argv: An array of strings containing the command-line
  * Return: 0 success
  */
 
-int _new_name(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	_putchar(argv[0]);
-	_putchar('\n');
-
+	printf("%s\n", *argv);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar(char c);
-int _new_name(int argc, char *argv[]);
+int _putchar(char c);
 
 #endif
