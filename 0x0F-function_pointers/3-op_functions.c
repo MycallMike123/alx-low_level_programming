4d01463092630763e615ee5dc29c36e5d746feeb
Michael Mwangi <michaelmichire2016@gmail.com>
2023-09-13 03:40:15 -0700

    The c files with the calc functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: First int
+ * @b: Second int
+ * Return: Sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference
+ * @a: First int
+ * @b: Second int
+ * Return: Difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product
+ * @a: First int
+ * @b: Second int
+ * Return: Product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns divisionof a & b
+ * @a: First int
+ * @b: Second int
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b)
+}
+
+/**
+ * op_mod - returns remainder
+ * @a: First int
+ * @b: Second int
+ * Return: Remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
