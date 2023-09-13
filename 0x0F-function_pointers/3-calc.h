d46ed6bb130f8054ebb2923d8fd79a5706b2e303
Michael Mwangi <michaelmichire2016@gmail.com>
2023-09-13 03:53:31 -0700

    The calc header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,8 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stddef.h>
+
 /**
  * struct op - Struct op
  *
@@ -13,6 +15,7 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
