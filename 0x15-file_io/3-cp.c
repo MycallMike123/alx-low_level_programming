a36bdcf5575f10ad271acb8611c5bc43272201aa
Michael Mwangi <michaelmichire2016@gmail.com>
2023-10-02 06:34:50 -0700

    A program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
 
@@ -9,75 +9,81 @@ int file_copy(const char *source_filename, const char *dest_filename);
  * main - Copies the content of one file to another
  * @argc: The number of command-line arguments
  * @argv: An array of command-line argument (strings)
- * Return: 0 on success
+ * Return: 0 success
  */
 
 int main(int argc, char *argv[])
 {
+	int res;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	if (file_copy(argv[1], argv[2]) != 0)
+	res = file_copy(argv[1], argv[2]);
+
+	switch (res)
 	{
-		exit(99);
+		case 1:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		case 2:
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			exit(99);
+		case 3:
+			dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+			exit(100);
 	}
 
 	return (0);
 }
 
 /**
- * file_copy - Copy the contents of one file to another
+ * file_copy - Copy the content of one file to another file
  * @source_filename: The name of the source file
  * @dest_filename: The name of the destination file
- * Return: 0 success, 1 failure
+ * Return: 0 success
  */
 
-int copy_file(const char *source_filename, const char *dest_filename)
+int file_copy(const char *source_filename, const char *dest_filename)
 {
-	int f1, f2, bytes_read, bytes_written;
+	int file1, file2, read_bytes, bytes_written;
 	char buf[BUFFER_SIZE];
 
-	f1 = open(source_filename, O_RDONLY);
-	if (f1 == -1)
+	file1 = open(source_filename, O_RDONLY);
+	if (file1 == -1)
+		return (1);  /* if reading error */
+
+	file2 = open(dest_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_filename);
-		exit(98);
+		close(file1);
+		return (2);
 	}
 
-	f2 = open(dest_filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (f2 == -1)
+	while ((read_bytes = read(file1, buf, BUFFER_SIZE)) > 0)
 	{
-		close(f1);
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_filename);
-		exit(99);
+		bytes_written = write(file2, buf, read_bytes);
+		if (bytes_written == -1 || bytes_written != read_bytes)
+		{
+			close(file1);
+			close(file2);
+			return (2);
+		}
 	}
 
-	do {
-		bytes_read = read(f1, buf, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			close(f1);
-			close(f2);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_filename);
-			exit(98);
-		}
-
-		bytes_written = write(f2, buf, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			close(f1);
-			close(f2);
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_filename);
-			exit(99);
-		}
-	} while (bytes_read > 0);
-	if (close(f1) == -1 || close(f2) == -1)
+	if (read_bytes == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(f1) == -1) ? f1 : f2);
-		exit(100);
+		close(file1);
+		close(file2);
+		return (1);
+	}
+
+	if (close(file1) == -1 || close(file2) == -1)
+	{
+		return (3);
 	}
 
 	return (0);
