441839fc1b9d6d8e6095c4ae9b43b9b7ef92d626
Michael Mwangi <michaelmichire2016@gmail.com>
2023-10-30 04:10:26 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		res = write(file_descriptor, text_content, strlen(text_content));
-	
+
 
 		if (res == -1 || (size_t)res != strlen(text_content))
 		{
